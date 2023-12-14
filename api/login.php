38b31c73fae168fd795d2ab02cf178ac1f3e5457
franztaub <109521780+franztaub@users.noreply.github.com>
2023-12-14 06:28:35 +0000

    commit

--- api/login.php
+++ api/login.php
@@ -24,99 +24,95 @@ $password = $_POST['pass'];
 $user = $collection->findOne(['username' => $username, 'password' => $password]);
 
 if ($user) {
-    // User is authenticated
-    echo '
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>Blue Background Page</title>
-      <style>
-        body {
-          background: linear-gradient(to bottom, #002e84, #5e8bda); /* Gradient color */
-          width: 100%;
-          height: 100%;
-          color: white;
-          font-family: "Times New Roman", Times, serif; /* Times New Roman font */
-          margin: 0;
-          font-size:30px;
-          padding: 20px;
-          text-align: center; /* Center align the content */
-        }
-        /* Style the button separately */
-        button {
-          padding: 30px 45px; /* Larger padding to increase button size */
-          background-color: #ffffff;
-          font-color: #1e3c72;
-          border-radius: 20px;
-          cursor: pointer;
-          text-decoration: none; /* Remove underline on hover */
-          font-size: 28px; /* Increase font size */
-          margin-top: 20px; /* Add margin to the button */
-        }
-        /* Style the button hover effect */
-        button:hover {
-          background-color: #f0f0f0;
-        }
-      </style>
-    </head>
-    <body>
-      <br><br><br><br><br><br><br><br><br><br><br><br><br>
-      <h1 style="color:black;"> Login successful! </h1>
-      <!-- Use a div or a different container instead of a button inside an anchor tag -->
-      <div>
-        <a href="../api/index.php" >
-          <button>Proceed</button>
-        </a>
-      </div>
-    </body>
-    </html>
-            ';
+  // User is authenticated
+  echo '
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Blue Background Page</title>
+    <style>
+      body {
+        background-color: #ADD8E6; /* Light blue background */
+        color: white;
+        font-family: "Times New Roman", Times, serif; /* Times New Roman font */
+        margin: 0;
+        padding: 20px;
+        text-align: center; /* Center align the content */
+      }
+      /* Style the button separately */
+      button {
+        padding: 20px 35px; /* Larger padding to increase button size */
+        background-color: #ffffff;
+        color: #0077b6;
+        border: none;
+        cursor: pointer;
+        text-decoration: none; /* Remove underline on hover */
+        font-size: 18px; /* Increase font size */
+        margin-top: 20px; /* Add margin to the button */
+      }
+      /* Style the button hover effect */
+      button:hover {
+        background-color: #f0f0f0;
+      }
+    </style>
+  </head>
+  <body>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <h1 style="color:black;"> Login successful! </h1>
+    <!-- Use a div or a different container instead of a button inside an anchor tag -->
+    <div>
+      <a href="../api/index.php" >
+        <button>Proceed</button>
+      </a>
+    </div>
+  </body>
+  </html>
+          ';
 } else {
-    // Invalid login
-    echo '
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <title>Blue Background Page</title>
-      <style>
-        body {
-          background-color: #2a5298; /* Light blue background */
-          color: white;
-          font-family: "Times New Roman", Times, serif; /* Times New Roman font */
-          margin: 0;
-          font-size:30px;
-          padding: 20px;
-          text-align: center; /* Center align the content */
-        }
-        /* Style the button separately */
-        button {
-          padding: 30px 45px; /* Larger padding to increase button size */
-          background-color: #ffffff;
-          font-color: #1e3c72;
-          border-radius: 20px;
-          cursor: pointer;
-          text-decoration: none; /* Remove underline on hover */
-          font-size: 28px; /* Increase font size */
-          margin-top: 20px; /* Add margin to the button */
-        }
-        /* Style the button hover effect */
-        button:hover {
-          background-color: #f0f0f0;
-        }
-      </style>
-    </head>
-    <body>
-      <br><br><br><br><br><br><br><br><br><br><br><br><br>
-      <h1 style="color:black;"> Invalid username or password. </h1>
-      <!-- Use a div or a different container instead of a button inside an anchor tag -->
-      <div>
-        <a href="../index.html" >
-          <button>Go Back</button>
-        </a>
-      </div>
-    </body>
-    </html>
-            ';
+  // Invalid login
+  echo '
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Blue Background Page</title>
+    <style>
+      body {
+        background-color: #ADD8E6; /* Light blue background */
+        color: white;
+        font-family: "Times New Roman", Times, serif; /* Times New Roman font */
+        margin: 0;
+        padding: 20px;
+        text-align: center; /* Center align the content */
+      }
+      /* Style the button separately */
+      button {
+        padding: 20px 35px; /* Larger padding to increase button size */
+        background-color: #ffffff;
+        color: #0077b6;
+        border: none;
+        cursor: pointer;
+        text-decoration: none; /* Remove underline on hover */
+        font-size: 18px; /* Increase font size */
+        margin-top: 20px; /* Add margin to the button */
+      }
+      /* Style the button hover effect */
+      button:hover {
+        background-color: #f0f0f0;
+      }
+    </style>
+  </head>
+  <body>
+    <br><br><br><br><br><br><br><br><br><br><br><br><br>
+    <h1 style="color:black;"> Invalid username or password. </h1>
+    <!-- Use a div or a different container instead of a button inside an anchor tag -->
+    <div>
+      <a href="../index.html" >
+        <button>Go Back</button>
+      </a>
+    </div>
+  </body>
+  </html>
+          ';
 }
